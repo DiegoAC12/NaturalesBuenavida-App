64af12b558f29b8a0a3ba77785372a72b9de5c85
DiegoAC12 <diegoalzate.0207@gmail.com>
2024-10-21 21:58:52 -0500

    Modificando comentarios en la clase BuyLog

--- WebApp_NaturalesBuenavida/Logic/BuyLog.cs
+++ WebApp_NaturalesBuenavida/Logic/BuyLog.cs
@@ -11,18 +11,18 @@ namespace Logic
     {
         BuyDat objBuy = new BuyDat();
 
-        //Metodo para mostrar todas las Categorias
+        //Metodo para mostrar todas las Compras
         public DataSet showBuy()
         {
             return objBuy.showBuy();
         }
 
-        //Metodo para guardar una nueva Categoria
+        //Metodo para guardar una nueva Compra
         public bool saveBuy(DateTime _fecha_compra, double _total, int _fkproducto_id, string _numero_factura, int _cantidad, double _precio_unitario)
         {
             return objBuy.saveBuy(_fecha_compra, _total, _fkproducto_id, _numero_factura, _cantidad, _precio_unitario);
         }
-        //Metodo para actualizar una Categoria
+        //Metodo para actualizar una Compra
         public bool updateBuy(int _compra_id, DateTime _fecha_compra, double _total, string _numero_factura, int _fkproducto_id, int _cantidad, double _precio_unitario)
         {
             return objBuy.updateBuy(_compra_id,_fecha_compra, _total, _numero_factura, _fkproducto_id, _cantidad, _precio_unitario);
